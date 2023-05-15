ead8039b76cfe2665bf86d1de3a26673288341f1
hongmee <janet195@naver.com>
2023-05-15 22:57:54 +0900

    lombok 연결

---
+++ forStudySite/src/main/java/kr/ong/mee/user/model/vo/User.java
@@ -0,0 +1,12 @@
+package kr.ong.mee.user.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+	private int userNo;
+}
